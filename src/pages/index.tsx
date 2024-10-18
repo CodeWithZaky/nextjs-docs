fbc192bdbe3d7c38bb9a927068a48bd74ede2d50
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2024-10-18 21:01:14 +0700

    add useRef

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,18 @@
-import localFont from "next/font/local";
 import Link from "next/link";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { Fragment } from "react";
 
 export default function Home() {
+  const LINK_DATA = ["useCallback", "useRef"];
+
   return (
-    <div className="flex justify-center items-center w-full h-screen font-semibold text-xl">
-      <button className="bg-slate-700 hover:bg-slate-800 px-3 py-1 rounded-xl">
-        <Link href="useCallback">useCallback</Link>
-      </button>
+    <div className="flex justify-center items-center gap-5 w-full h-screen font-semibold text-xl">
+      {LINK_DATA.map((item, index) => (
+        <Fragment key={index}>
+          <button className="bg-slate-700 hover:bg-slate-800 px-3 py-1 rounded-xl">
+            <Link href={item}>{item}</Link>
+          </button>
+        </Fragment>
+      ))}
     </div>
   );
 }
